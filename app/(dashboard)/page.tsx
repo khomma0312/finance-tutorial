879b28bf719560ddcf1ebe3bf8caed019d0fe058
khomma0312 <kijikuroneko@gmail.com>
2024-09-16 08:37:40 +0900

    [fix] Delete unnecessary comments

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-// import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 
 export default function Home() {
   const { onOpen } = useNewAccount();
-  // const { data: accounts, isLoading } = useGetAccounts();
 
   return (
     <div>
