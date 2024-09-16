415e149121e1b07f5befce7f1b0f30432b2ab32f
khomma0312 <kijikuroneko@gmail.com>
2024-09-16 12:58:18 +0900

    [feat]Add transactions API

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 import accounts from "./accounts";
 import categories from "./categories";
+import transactions from "./transactions";
 
 export const runtime = "edge";
 const app = new Hono().basePath("/api");
@@ -9,7 +10,8 @@ const app = new Hono().basePath("/api");
 // https://hono.dev/docs/guides/rpc#using-rpc-with-larger-applications
 const routes = app
   .route("/accounts", accounts)
-  .route("/categories", categories);
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,7 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const accounts = pgTable("accounts", {
   id: text("id").primaryKey(),
@@ -7,6 +9,10 @@ export const accounts = pgTable("accounts", {
   name: text("name").notNull(),
   userId: text("user_id").notNull(),
 });
+
+export const acccountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions),
+}));
 
 export const insertAccountSchema = createInsertSchema(accounts);
 
@@ -17,4 +23,41 @@ export const categories = pgTable("categories", {
   userId: text("user_id").notNull(),
 });
 
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
 export const insertCategorySchema = createInsertSchema(categories);
+
+// テーブルのマイグレーションのための設定
+export const transactions = pgTable("transactions", {
+  id: text("id").primaryKey(),
+  amount: integer("amount").notNull(), // amountは*1000して保存することで、小数点以下3桁まで保存できる(floatなどは計算結果に誤差が出るため)
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id")
+    .references(() => accounts.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+// ORMでRelation先のデータを持って来れるようにするための設定？
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  category: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(), // 指定した型に変換して保存する
+});
